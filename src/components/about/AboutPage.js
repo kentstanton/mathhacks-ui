b890ed2117e098fb136edbc89bf98b76f8dd97cb
kentstanton <kent.stanton@gmail.com>
2017-12-09 07:51:21 -0500

    Add event handler for answers

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -5,36 +5,47 @@ const tex = `f(x) = \\int_{-\\infty}^\\infty
 \\hat f(\\xi)\\,e^{2 \\pi i \\xi x}
 \\,d\\xi`
 
-const asciimath = `x^-3`
+const question = `(e^2)^2`;
+
+const answer1 = `e*e*e*e`;
+const answer2 = `2*(e^2)`;
+const answer3 = `4*e`;
+const answer4 = `e^4`;
 
 
 class AboutPage extends React.Component {
     render() {
 
-      function handleClick(e) {
-        alert('The link was clicked.');
+      function handleClick(ans, e) {
+        if (ans) {
+          alert("Correct");
+        } else {
+          alert("Not Correct");
+        }
+
       }
+
       return (
           <MathJax.Context>
             <div>
               <div className="mathquestionrow">
-                  <MathJax.Node inline>{`(e^2)^2`}</MathJax.Node>
+                  <MathJax.Node inline>{question}</MathJax.Node>
               </div>
                   <div >Click on each of the equivalent expressions below:</div>
 
                   <div className="mathanswerblock">
 
-                  <div className="mathanswerrow" onClick={handleClick} >
-                  <MathJax.Node>{`e*e*e*e`}</MathJax.Node>
+                  <div className="mathanswerrow" onClick={handleClick.bind(this, 1)} >
+                  <MathJax.Node>{answer1}</MathJax.Node>
                   </div>
-                  <div className="mathanswerrow">
-                  <MathJax.Node>{`2*(e^2)`}</MathJax.Node>
+                  <div className="mathanswerrow" onClick={handleClick.bind(this, 1)}>
+                  <MathJax.Node>{answer2}</MathJax.Node>
                   </div>
-                  <div className="mathanswerrow">
-                  <MathJax.Node>{`4*e`}</MathJax.Node>
+                  <div className="mathanswerrow" onClick={handleClick.bind(this, 0)}>
+                  <MathJax.Node>{answer3}</MathJax.Node>
                   </div>
-                  <div className="mathanswerrow">
-                  <MathJax.Node>{`e^4`}</MathJax.Node>
+                  <div className="mathanswerrow" onClick={handleClick.bind(this, 0)}>
+                  <MathJax.Node>{answer4}</MathJax.Node>
                   </div>
 
                   </div>
